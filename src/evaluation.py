32338709e190bcbcd191d8d606f8cdf231471a18
Alan Hou <houych@gmail.com>
2017-09-10 22:23:28 -0700

    add grid search feature

--- src/evaluation.py
+++ src/evaluation.py
@@ -7,13 +7,16 @@ import tensorflow
 from tensorflow.contrib import learn
 
 
-def make_data_files(embeddings_file, links_file, training_file, testing_file):
+def make_data_files(data_set_name, training_file, testing_file):
+    embeddings_file = '../emb/' + data_set_name + '.emb'
+    links_file = '../graph/' + data_set_name + '.tsv'
     embeddings = pandas.read_csv(embeddings_file, sep=' ', header=None, index_col=0, skiprows=1)
     embeddings.sort_index(inplace=True)
     links = pandas.read_csv(links_file, sep='\t', header=None)
 
     def embed(node_id):
         return ', '.join(str(element) for element in embeddings.loc[[node_id]].values.flatten())
+
     links[0] = links[0].map(embed)
     links[1] = links[1].map(embed)
     links[2] = preprocessing.MaxAbsScaler().fit_transform(numpy.log(links[2].values.reshape(-1, 1)))
@@ -22,37 +25,44 @@ def make_data_files(embeddings_file, links_file, training_file, testing_file):
     testing_links.to_csv(testing_file, index=False, header=False, quotechar=' ')
 
 
-def input_fn(data_set):
+def input_fn(data_set, num_epochs, ):
     return tensorflow.estimator.inputs.numpy_input_fn(
-        {'x': numpy.array(data_set.data)}, numpy.array(data_set.target), shuffle=False
+        {'x': numpy.array(data_set.data)}, numpy.array(data_set.target), num_epochs=num_epochs, shuffle=False,
     )
 
 
-def evaluate(model_dir, training_file, testing_file, dimension):
+def evaluate(data_set_name, training_file, testing_file, dimension, num_epochs, trial):
+    model_dir = os.path.join('../log', data_set_name, str(num_epochs), str(dimension), str(trial))
+    if os.path.exists(model_dir):
+        shutil.rmtree(model_dir)
     training_set = learn.datasets.base.load_csv_without_header(training_file, numpy.float, numpy.float)
     testing_set = learn.datasets.base.load_csv_without_header(testing_file, numpy.float, numpy.float)
     model = tensorflow.estimator.DNNRegressor(
-        [dimension, dimension], [tensorflow.feature_column.numeric_column('x', [2 * dimension])], model_dir
+        [dimension, dimension, ], [tensorflow.feature_column.numeric_column('x', [2 * dimension])], model_dir
     )
-    model.train(input_fn(training_set))
-    predictions = list(model.predict(input_fn(testing_set)))
+    model.train(input_fn(training_set, num_epochs, ))
+    predictions = list(model.predict(input_fn(testing_set, 1, )))
     actual_target = numpy.concatenate([prediction['predictions'] for prediction in predictions])
     return metrics.mean_squared_error(actual_target, testing_set.target)
 
 
-def main(data_set_name):
-    dimension = 4
-    model_dir = '../log/' + data_set_name
-    training_file = '../emb/' + data_set_name + '_training.csv'
-    testing_file = '../emb/' + data_set_name + '_testing.csv'
-    embeddings_file = '../emb/' + data_set_name + '.emb'
-    links_file = '../graph/' + data_set_name + '.tsv'
-    if os.path.exists(model_dir):
-        shutil.rmtree(model_dir)
-    make_data_files(embeddings_file, links_file, training_file, testing_file)
-    print evaluate(model_dir, training_file, testing_file, dimension)
+def main():
+    errors = pandas.DataFrame(columns=['data_set_name', 'num_epochs', 'dimension', 'error', ])
+    # for data_set_name in ['airport', 'collaboration', 'congress', 'forum',]:
+    for data_set_name in ['airport', ]:
+        training_file = os.path.join('../emb', data_set_name + '_training.csv')
+        testing_file = os.path.join('../emb', data_set_name + '_testing.csv')
+        for num_epochs in range(1, 11):
+            for dimension in [4, ]:
+                make_data_files(data_set_name, training_file, testing_file)
+                error = numpy.mean([
+                    evaluate(
+                        data_set_name, training_file, testing_file, dimension, num_epochs, trial
+                    ) for trial in range(10)
+                ])
+                errors.loc[len(errors)] = [data_set_name, num_epochs, dimension, error]
+    print(errors)
 
 
 if __name__ == '__main__':
-    for data_set_name in ['airport', 'collaboration', 'congress', 'forum']:
-        main(data_set_name)
+    main()
